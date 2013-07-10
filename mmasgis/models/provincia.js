9d6ea74633adb27d7ee602a5336ea3ea5b4303a5
giuseppe <damicogiuseppe77@gmail.com>
2013-07-11 01:29:06 +0200

    5.3.1 corretti i bachi del test, ora mancano gli ultimi passi  per ricavare i tc_istat_id

--- mmasgis/models/provincia.js
+++ mmasgis/models/provincia.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
 var ProvinciaSchema = require('../schemas/provincia');
-var Provincia = mongoose.model('provincia', ProvinciaSchema);
+var Provincia = mongoose.model('Provincia', ProvinciaSchema);
 module.exports = Provincia;
--- mmasgis/routes/middleware/tc_istat_id.js
+++ mmasgis/routes/middleware/tc_istat_id.js
@@ -1,11 +1,14 @@
 var Region = require('../../models/region');
 var Provincia = require('../../models/provincia');
+var User = require('../../models/user');
 function getProvince(regioni,next)
 /*
  * ritorna la lista dei cod_pro appartenenti alla lista delle regioni passate
  * @arg regioni:[{classe:'regione',id:int}]
  * @return [int]*/
 {
+	/*User.find({nome:'me'},function(err,user){
+		console.log(user)})*/
 	var regioni_id = []
 	console.log('getProvince')
 	console.log(regioni)
@@ -15,15 +18,23 @@ function getProvince(regioni,next)
 		regioni_id.push(Number(regioni[i].id))
 		//console.log(regioni_id)
 	}
-	console.log(regioni_id)
-	var province = null
-	Provincia.find({codice_regione:'p'},function(err,province){
+	//console.log(regioni_id)
+	//var province = null
+	//next('test next prima DI FIND','should be prov')
+	console.log('ora provincia.find')
+	Provincia.find({codice_regione:{$in:regioni_id}},function(err,province){
 		 if (err){console.log('errorre whoosh '+err);
-		 return next(err,province)
+		 console.log('this is callback')
+		//next(err,province)
 		 }
-		 if (!province){console.log('trovato nulla')}
-		 console.log('callback tc_istat_id')
-		 return next(err,province)
+		if (!province){console.log('trovato nulla')}
+		console.log('callback tc_istat_id')
+		var out =[]
+		for (var i=0;i<province.length;i++)
+		{
+			out.push(province[i].codice)
+		}
+		next(err,out)
 	 })
 	 //next()
 	 //console.log('dopo find')
